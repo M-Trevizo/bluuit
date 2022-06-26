f01f3a15382df9f8aabb64bce8338cd42484b9e4
Michael Trevizo <michael.trevizo93@gmail.com>
2022-06-26 13:15:57 -0700

    Complete NavBar styling for small screens

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -6,10 +6,14 @@ import searchImg from '../../media/search.svg';
 export const NavBar = () => {
     return (
         <nav>
-            <img id="logo" src={logo} alt="Reddit Logo"/>
-            <h1>Bluuit</h1>
-            <input type="search" id="search-field" placeholder="Search"></input>
-            <img id="search" src={searchImg} alt="Search" />
+            <div id="title">
+                <img id="logo-icon" src={logo} alt="Reddit Logo"/>
+                <h1>Bluuit</h1>
+            </div>
+            <div id="search">
+                <input type="search" id="search-field" placeholder="Search"></input>
+                <img id="search-icon" src={searchImg} alt="Search" />
+            </div>
         </nav>
     );
 }
