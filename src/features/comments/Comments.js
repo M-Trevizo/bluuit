cda8f9a1d66a01540f3b2546064258004356162c
Michael Trevizo <michael.trevizo93@gmail.com>
2022-08-28 18:42:58 -0700

    Comments styling

--- src/features/comments/Comments.js
+++ src/features/comments/Comments.js
@@ -1,5 +1,5 @@
 import './Comments.css';
-import commentsImg from '../../media/comments.svg';
+import commentsImg from '../../media/commentsLight.svg';
 
 export const Comments = (numComments) => {
     const comments = numComments.numComments
@@ -7,7 +7,7 @@ export const Comments = (numComments) => {
     return (
         <div className='comments'>
             <img id='comments-logo' className='info-item' src={commentsImg} alt='' />
-            <p className='info-item'>{comments}</p>
+            <p className='info-item'>{comments} comments</p>
         </div>
     );
 }
